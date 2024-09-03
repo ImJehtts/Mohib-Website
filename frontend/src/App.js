5ac7eeee386dc3b72981424eb7b74b9c4fb47d48
Mohib Rehman <rehmanmohib08@gmail.com>
2024-09-03 04:04:00 -0400

    First save
    Deleted all unnecessary/ I didn't need from creating the react.js project. Created my components.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header/Header'
+import Nav from './components/Nav/Nav'
+import About from './components/About/About'
+import Skills from './components/Skills/Skills'
+import School from './components/School/School'
+import Portfolio from './components/Portfolio/Portfolio'
+import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+        <Header/>
+        <Nav/>
+        <About/>
+        <Skills/>
+        <School/>
+        <Portfolio/>
+        <Contact/>
+        <Footer/>
+      </>
     </div>
   );
 }
---
+++ frontend/src/components/School/School.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './School.css'
+
+
+const School = () => {
+  return (
+    <div>
+      School
+    </div>
+  )
+}
+
+export default School
