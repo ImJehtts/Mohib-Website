140eedd957b1ccf1de7b1f32405babdf2cbf3b35
Mohib Rehman <rehmanmohib08@gmail.com>
2024-09-25 17:39:21 -0400

    Added Footer note about Mobile Support

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -7,6 +7,7 @@ const Footer = () => {
     <section id='footer__id'>
       <footer>
       <a href='#' className='footer__name'>Mohib Rehman</a>
+      <h4>(Proper Mobile View Soon)</h4>
       <ul className='footer__links'>
         <li><a href='#'>Home</a></li>
         <li><a href='#About'>About</a></li>
