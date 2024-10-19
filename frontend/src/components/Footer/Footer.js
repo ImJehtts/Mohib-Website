697ff04efd344b30042f0b094ee32323b112e599
Mohib Rehman <rehmanmohib08@gmail.com>
2024-10-19 19:42:21 -0400

    Website should look good on most devices now
    Worked on dynamic css for phones, tablets, and laptops. Will tweak if needed

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
     <section id='footer__id'>
       <footer>
       <a href='#' className='footer__name'>Mohib Rehman</a>
-      <h6>(Mobile View Support Soon)</h6>
+      <h6>(Working on responsive support for different devices)</h6>
       <ul className='footer__links'>
         <li><a href='#'>Home</a></li>
         <li><a href='#About'>About</a></li>
