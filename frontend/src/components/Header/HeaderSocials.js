4b1029e0f6a59347c3584de162776d30c74169a8
Mohib Rehman <111709491+ImJehtts@users.noreply.github.com>
2024-11-01 15:03:40 -0400

    Update linkedin link

--- frontend/src/components/Header/HeaderSocials.js
+++ frontend/src/components/Header/HeaderSocials.js
@@ -5,7 +5,7 @@ import {FaGithub} from 'react-icons/fa'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-      <a href='https://www.linkedin.com/in/mohib-rehman-892930245/' target='_blank'><BsLinkedin/></a>
+      <a href='https://www.linkedin.com/in/mohreh' target='_blank'><BsLinkedin/></a>
       <a href='https://github.com/ImJehtts' target='_blank'><FaGithub/></a>
     </div>
   )
