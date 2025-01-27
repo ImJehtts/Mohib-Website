d3f2def188af266fc03f0347a4d7a0b34ae9d617
Mohib Rehman <rehmanmohib08@gmail.com>
2025-01-27 00:50:45 -0500

    Revert "trying to fix mobile responsiveness"
    This reverts commit d58213822d756243ee421f0df570f1ffabe40a6e.

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -16,7 +16,6 @@ const Footer = () => {
         <li><a href='#Portfolio'>Portfolio</a></li>
         <li><a href='#Contact'>Contact</a></li>
       </ul>
-      <h6>(Working on responsive support for different devices. Tablets may not work as intented)</h6>
     </footer>
     </section>
   )
