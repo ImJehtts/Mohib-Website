d58213822d756243ee421f0df570f1ffabe40a6e
Mohib Rehman <rehmanmohib08@gmail.com>
2025-01-26 23:12:18 -0500

    trying to fix mobile responsiveness

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -16,6 +16,7 @@ const Footer = () => {
         <li><a href='#Portfolio'>Portfolio</a></li>
         <li><a href='#Contact'>Contact</a></li>
       </ul>
+      <h6>(Working on responsive support for different devices. Tablets may not work as intented)</h6>
     </footer>
     </section>
   )
